52913c5b727c0f1f72a3d2b424bf88d97595086d
NaflayM <nafmurimi@gmail.com>
2024-05-09 18:19:32 +0300

    added date function

--- WeatherAPP/views.py
+++ WeatherAPP/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
-
+import datetime
 # Create your views here.
 def index(request):
     # return HttpResponse("Weather Man")
-    return render(request, 'index.html')
+    current_datetime = datetime.datetime.now()
+    return render(request, 'index.html' ,{'current_datetime': current_datetime})
